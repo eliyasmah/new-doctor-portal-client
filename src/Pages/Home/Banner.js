58130b77b4bd1bed234e92a5f00871d782567b5c
eliyasmah <eliyasbubt@gmail.com>
2022-08-21 14:18:33 +0600

    contact us section

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -12,7 +12,7 @@ const Banner = () => {
       class="hero min-h-screen"
     >
       <div class="hero-content flex-col lg:flex-row-reverse">
-        <img src={chair} class="max-w-sm rounded-lg shadow-2xl" />
+        <img src={chair} class="max-w-sm rounded-lg shadow-2xl" alt="img" />
         <div>
           <h1 class="text-5xl font-bold">Your New Smile Starts Here</h1>
           <p class="py-6">
---
+++ src/Pages/Home/ContactUs.js
@@ -0,0 +1,41 @@
+import React from "react";
+import appointment from "../../assets/images/appointment.png";
+import PrimaryButton from "../Shared/PrimaryButton";
+
+const ContactUs = () => {
+  return (
+    <section
+      style={{
+        background: `url(${appointment})`,
+      }}
+      className="text-center py-14"
+    >
+      <div>
+        <h1 className="text-secondary font-bold text-xl">Contact Us</h1>
+        <h2 className="text-4xl text-white">Stay connected with us</h2>
+      </div>
+      <div className="grid grid-cols-1 gap-5 justify-items-center py-8">
+        <input
+          type="email"
+          placeholder="Email Address"
+          className="input w-full max-w-md"
+        />
+        <input
+          type="text"
+          placeholder="Subject"
+          className="input w-full max-w-md"
+        />
+        <textarea
+          name=""
+          id=""
+          rows="6"
+          placeholder="Your Message"
+          className="textarea w-full max-w-md"
+        ></textarea>
+        <PrimaryButton>Submit</PrimaryButton>
+      </div>
+    </section>
+  );
+};
+
+export default ContactUs;
